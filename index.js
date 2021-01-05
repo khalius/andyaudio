8c337e8fe67f3e72bca50f8af6372a9b06df31b7
andy <andyguachin87@gmail.com>
2021-01-05 11:09:23 -0300

    v 1.4

--- index.js
+++ index.js
@@ -4,24 +4,40 @@ const fs = require('fs-extra');
 const path = require('path');
 const multer = require('multer');
 const cors = require('cors');
-const dir = path.join(__dirname, 'audios');
+const publicDir = path.join(__dirname, 'public');
+const audiosDir = path.join(__dirname, 'public', 'audios');
+const imgsDir = path.join(__dirname, 'public', 'imgs');
 
-if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+if (!fs.existsSync(publicDir)) fs.mkdirSync(publicDir);
+if (!fs.existsSync(audiosDir)) fs.mkdirSync(audiosDir);
+if (!fs.existsSync(imgsDir)) fs.mkdirSync(imgsDir);
 
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'audios')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
     origin: 'https://chat.elchat.net'
 }));
 
-app.post('/upAudio', multer().single('archivo'), (req, res) => {
+app.post('/upAudio', multer().single('audio'), (req, res) => {
     if (req.file.size > 1024 * 1024) return null;
     if (req.file.mimetype !== 'audio/ogg') return null;
     let date = Date.now().toString();
-    fs.writeFile(`${path.join(__dirname, 'audios')}/${date}.ogg`, req.file.buffer)
-        .then(() => console.log('archivo subido'))
+    fs.writeFile(`${audiosDir}/${date}.ogg`, req.file.buffer)
+        .then(() => console.log('Audio subido'))
         .catch(err => console.log(err));
     res.send(`${date}.ogg`);
+});
+
+app.post('/upImg', multer().single('image'), (req, res) => {
+    if (req.file.size > 1024 * 1024 * 5) return null;
+    const regex = /(png|jpg|jpeg|gif)/;
+    let searchMimeType = req.file.mimetype.match(regex);
+    if (searchMimeType == null) return null;
+    let date = Date.now().toString();
+    fs.writeFile(`${imgsDir}/${date}.${searchMimeType[0]}`, req.file.buffer)
+        .then(() => console.log('Imagen subida'))
+        .catch(err => console.log(err));
+    res.send(`${date}.${searchMimeType[0]}`);
 });
 
 app.get('/AndyAudio', (req, res) => {
--- lala.js
+++ lala.js
@@ -6,27 +6,26 @@ async function main() {
     const mediaRecorder = new MediaRecorder(stream);
     let state = false;
     let chunks = [];
-    let stateImg = true;
     let serverPath = `https://andyaudio.herokuapp.com`;
     let falseStyle = { width: '30px', height: '30px', borderRadius: '9999px', background: '#51ce86', display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer' };
     const audioBtn = `<div id="audioBtn">
         <i class="fa fa-microphone" />
         <p id="audioTxt"></p>
     </div>`;
-    const imgBtn = `<i class="fa fa-picture-o" id="imgUpBtn" style="margin-left:10px;cursor:pointer;"></i>`;
-    const urlImg = `<div id="boxUrlImg" style="display:flex;width:80%;margin:auto">
-        <input type="text" maxlength="200" placeholder="Ingrese el LINK de la imágen" style="background:#e1fcff;border:none;width:100%;border-bottom-left-radius:20px;border-top-left-radius:20px;font-weight:bold;"/>
-        <button style="border:none;background:#51ce86;border-bottom-right-radius:20px;border-top-right-radius:20px;font-weight:bold;" id="btnUpImg">Enviar</button>
-    </div>`
+    const imgBtn = `<div>
+        <input type="file" style="display:none;" id="inputImg"  accept="image/*"/>
+        <label for="inputImg">
+            <i class="fa fa-picture-o" style="margin-left:10px;cursor:pointer;"></i>
+        </label>
+    </div>`;
     $('.textarea-icons-wrapper').append(audioBtn);
     $('.textarea-icons-wrapper').append(imgBtn);
-    $('#marqueeContainer').append(urlImg);
     $('#audioBtn').css(falseStyle);
     x.receiveText = (a, b, c) => {
         let z = /^\[audio\]/i;
         let path = b.replace(z, '');
         if (z.test(b)) {
-            old.apply(x, [a, `<audio controls src="${serverPath}/${path}"><audio>`, c])
+            old.apply(x, [a, `<audio controls src="${serverPath}/audios/${path}"><audio>`, c])
             return;
         }
         old.apply(x, [a, b, c])
@@ -43,25 +42,31 @@ async function main() {
             state = false;
         }
     });
-    $('#imgUpBtn').click(e => {
-        if (!stateImg) {
-            $('#boxUrlImg').show('slow');
-            stateImg = true;
-            return null;
-        } else {
-            $('#boxUrlImg').hide('slow');
-            stateImg = false;
+    $('#inputImg').change(e => {
+        let file = e.target.files[0];
+        if (file.size > 1024 * 1024 * 5) {
+            alert('La imágen es muy pesada');
             return null;
         }
-
-    });
-    $('#btnUpImg').click(e => {
-        let val = $('#boxUrlImg input').val();
-        if (!val || val.length <= 0) return null;
-        let valParsed = val.replace(/https?\:/, '');
-        x.emojiArea[0].emojioneArea.setHTML(`<img class='gif' src='${valParsed}'/>`);
-        x.sendText();
-        $('#boxUrlImg input').val('');
+        const regex = /(png|jpg|jpeg|gif)/;
+        if (!regex.test(file.type)) {
+            alert('Solo se aceptan imágenes');
+            return null;
+        }
+        let form = new FormData();
+        form.append('image', file);
+        $.ajax({
+            url: `${serverPath}/upImg`,
+            type: 'POST',
+            data: form,
+            processData: false,
+            contentType: false,
+            success: response => {
+                x.emojiArea[0].emojioneArea.setHTML(`<img class="gif" src="${serverPath}/imgs/${response}"/>`);
+                x.sendText();
+            },
+            error: err => console.log(err)
+        });
     });
     mediaRecorder.ondataavailable = e => {
         chunks.push(e.data);
@@ -70,7 +75,7 @@ async function main() {
         let blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
         chunks = [];
         let form = new FormData();
-        form.append('archivo', blob);
+        form.append('audio', blob);
         $.ajax({
             url: `${serverPath}/upAudio`,
             type: 'POST',
@@ -81,8 +86,8 @@ async function main() {
                 x.emojiArea[0].emojioneArea.setText(`[audio]${response}`);
                 x.sendText();
             },
-            error: e => console.log(e)
-        })
+            error: err => console.log(err)
+        });
     }
 
 }
