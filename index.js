07aaefe8a23f847164f23cd522ea8058dd3d533e
andy <andyguachin87@gmail.com>
2021-01-12 19:10:57 -0300

    v 3.1.3

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ app.post('/upImg', multer().single('image'), (req, res) => {
 
 app.get('/', (req, res) => {
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(typeof(ip));
     if (/196\.52\.43/.test(ip)) {
+        console.log('bloqueado : ', ip);
         res.end();
         return;
     }
